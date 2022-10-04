80b26aad2583e37af84b55ecd348ffa487df19e5
Ueverton Souza <88385921+uevertonsouza@users.noreply.github.com>
2022-10-04 19:53:05 -0300

    Delete arquivos-aula directory

--- arquivos-aula/Exercicios/Script_Exemplo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Este é um Script de Teste"
-echo " " 
-date
-echo " "
-echo "Fim do Script"
--- arquivos-aula/Exercicios/Script_Variavel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "O script le e imprime o valor da variavel TESTE"
-echo " " 
-echo "O valor da variavel TESTE é:" $TESTE
-
-
